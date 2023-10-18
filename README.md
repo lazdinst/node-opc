ba223d1c4614c5b2feb6a35591cf342075254573
Talis Lazdins <nauda@Taliss-MacBook-Pro.local>
2023-10-18 08:34:28 -0400

    browsing nodes

--- README.md
+++ README.md
@@ -4,3 +4,9 @@ https://github.com/node-opcua/node-opcua-pki
 https://github.com/node-opcua/node-opcua/issues/570
 `npx node-opcua-pki createPKI`
 `npx node-opcua-pki certificate --ip="0.0.0.0" -a 'urn:{localhost}:Node-OPCUA-Client' --selfSigned -o cert.pem`
+
+12:12:40.772Z :verify :48 [NODE-OPCUA-W06] The certificate subjectAltName does not match the client applicationUri
+... Please regenerate a specific certificate that matches your client applicationUri
+... certificate subjectAltName = urn:{localhost}:Node-OPCUA-Client
+... client applicationUri = urn:Taliss-MacBook-Pro.local:NodeOPCUAClient
+... certificateFile = ./certificates/PKI/own/certs/cert.pem
--- opcuaClient.js
+++ opcuaClient.js
@@ -8,19 +8,19 @@ const SERVER_ENDPOINT = "opc.tcp://localhost:50000";
 const CERT_PATH = "./certificates/PKI/own/certs/cert.pem";
 const KEY_PATH = "./certificates/PKI/own/private/private_key.pem";
 
-app.get("/connect", async (req, res) => {
-  const options = {
-    applicationName: "NodeOPCUAClient",
-    connectionStrategy: {
-      timeout: 5000,
-    },
-    securityMode: opcua.MessageSecurityMode.SignAndEncrypt,
-    securityPolicy: opcua.SecurityPolicy.Basic256Sha256,
-    endpointMustExist: false,
-    certificateFile: CERT_PATH,
-    privateKeyFile: KEY_PATH,
-  };
+const options = {
+  applicationName: "NodeOPCUAClient",
+  connectionStrategy: {
+    timeout: 5000,
+  },
+  securityMode: opcua.MessageSecurityMode.SignAndEncrypt,
+  securityPolicy: opcua.SecurityPolicy.Basic256Sha256,
+  endpointMustExist: false,
+  certificateFile: CERT_PATH,
+  privateKeyFile: KEY_PATH,
+};
 
+app.get("/connect", async (req, res) => {
   const client = opcua.OPCUAClient.create(options);
 
   try {
@@ -29,6 +29,40 @@ app.get("/connect", async (req, res) => {
     res.send("Connected successfully to the OPC Server.");
   } catch (err) {
     res.send(`Failed to connect: ${err.message || err}`);
+  }
+});
+
+app.get("/browse", async (req, res) => {
+  const client = opcua.OPCUAClient.create(options);
+
+  try {
+    await client.connect(SERVER_ENDPOINT);
+
+    // Create session
+    const session = await client.createSession();
+
+    // Browse the root folder
+    const browseResult = await session.browse("RootFolder");
+
+    // Disconnect session and client after browsing
+    await session.close();
+    await client.disconnect();
+
+    // Convert browse results to JSON
+    const nodes = browseResult.references.map((reference) => {
+      return {
+        nodeId: reference.nodeId.toString(),
+        browseName: reference.browseName.toString(),
+        nodeClass: opcua.NodeClass[reference.nodeClass],
+        typeDefinition: reference.typeDefinition
+          ? reference.typeDefinition.toString()
+          : null,
+      };
+    });
+
+    res.json(nodes);
+  } catch (err) {
+    res.send(`Failed to browse: ${err.message || err}`);
   }
 });
 
